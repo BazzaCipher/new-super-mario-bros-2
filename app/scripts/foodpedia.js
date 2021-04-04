51bcdc254bebc82e54fa95e37bd9a86e52b5c5fc
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2021-04-05 06:51:34 +1000

    i'm freaking out rn

--- app/scripts/foodpedia.js
+++ app/scripts/foodpedia.js
@@ -4,6 +4,17 @@ import "firebase/firestore";
 import "./auth";
 import "./firebase";
 
+var firebaseConfig = {
+    apiKey: "AIzaSyC8iAiHIr6fK5U0xX1lW5GzLZF3HZfySRs",
+    authDomain: "hackiethon-food-photo-journal.firebaseapp.com",
+    projectId: "hackiethon-food-photo-journal",
+    storageBucket: "hackiethon-food-photo-journal.appspot.com",
+    messagingSenderId: "724215158434",
+    appId: "1:724215158434:web:0a28ad3a6af4124eba29fb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 const db = firebase.firestore().collection('foodimals')
 console.log(db.path)
 
