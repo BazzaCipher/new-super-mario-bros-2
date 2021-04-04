7de8e5f4a5f93197a45b1f9278a92ac70397f737
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2021-04-05 04:00:11 +1000

    Replace with online solution

--- app/scripts/foodpedia.js
+++ app/scripts/foodpedia.js
@@ -1,24 +1,21 @@
 import firebase from "firebase/app";
 
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", () => {
     if (!window.location.pathname.includes("foodPedia")) return
 
     let table = document.querySelector("#foodpediaTable")
 
     const db = firebase.firestore()
+    const foodimallist = db.collection('foodimals').listDocuments()
 
-    db.collection('foodimals')
-
-    let res = await fetch("/assets/foods/foodlist.json");
-    let json = await res.json();
-    for (let food of json.foods) {
+    for (let foodimal of foodimallist) {
         let row = document.createElement("tr");
         let imgCell = document.createElement("td");
         let image = new Image();
         image.width = 100;
         image.src = "/assets/QuestionMark.png"
         let text = document.createElement("td");
-        text.innerText = food;
+        text.innerText = foodimal.get().get('description');
         imgCell.appendChild(image)
         table.appendChild(row);
         row.appendChild(imgCell);
